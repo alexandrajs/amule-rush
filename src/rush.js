ce45e78c45e7a72072c05d9d5a517f0d9c21b2b2
Michał Żaloudik <ponury.kostek@gmail.com>
2018-05-14 16:06:56 +0200

    code refactor  - use AMule.Layer  - class-based

--- src/rush.js
+++ src/rush.js
@@ -4,139 +4,84 @@
  */
 const Redis = require("ioredis");
 const fast = require("fast.js");
+const Layer = require("amule").Layer;
 
 /**
  *
  * @constructor
  */
-function Rush(options) {
-	options = fast.assign({prefix: ""}, options || {});
+class Rush extends Layer {
+	constructor(options) {
+		super();
+		options = fast.assign({prefix: ""}, options || {});
+		this.client = options.client instanceof Redis ? options.client : new Redis(options.client);
+		this.prefix = options.prefix || "";
+		this.ttl = parseInt(options.ttl, 10) || 0;
+	}
+
 	/**
 	 *
-	 * @type {object}
+	 * @param key
+	 * @param field
+	 * @param callback
 	 */
-	this.next = null;
+	_has(key, field, callback) {
+		this.client.hget(this.prefix + key, field, (err, value) => {
+			if (err || typeof value !== "string") {
+				return callback(err, false);
+			}
+			try {
+				JSON.parse(value);
+				callback(err, true);
+			} catch (err) {
+				callback(err, false);
+			}
+		});
+	};
+
 	/**
 	 *
+	 * @param key
+	 * @param field
+	 * @param callback
 	 */
-	this.client = options.client instanceof Redis ? options.client : new Redis(options.client);
-	this.prefix = options.prefix || "";
-	this.ttl = parseInt(options.ttl, 10) || 0;
-	this.clearStats();
+	_get(key, field, callback) {
+		this.client.hget(this.prefix + key, field, (err, value) => {
+			callback(err, typeof value === "string" ? JSON.parse(value) : value);
+		});
+	};
+
+	/**
+	 *
+	 * @param key
+	 * @param field
+	 * @param value
+	 * @param callback
+	 */
+	_set(key, field, value, callback) {
+		this.client.hset(this.prefix + key, field, JSON.stringify(value), callback);
+	};
+
+	/**
+	 *
+	 * @param key
+	 * @param field
+	 * @param callback
+	 */
+	_delete(key, field, callback) {
+		this.client.hdel(this.prefix + key, field, callback);
+	};
+
+	/**
+	 * @param callback
+	 */
+	_clear(callback) {
+		const stream = this.client.scanStream({match: this.prefix + "*"});
+		stream.on("data", (keys) => {
+			this.client.del(keys.join(" "));
+		});
+		stream.on("end", () => callback(null, true));
+	};
 }
 
-/**
- *
- * @param key
- * @param field
- * @param callback
- */
-Rush.prototype.has = function (key, field, callback) {
-	this.client.hget(this.prefix + key, field, (err, value) => {
-		if (err || value === null) {
-			return callback(null, false);
-		}
-		callback(null, true);
-	});
-};
-/**
- *
- * @param key
- * @param field
- * @param callback
- */
-Rush.prototype.get = function (key, field, callback) {
-	this.client.hget(this.prefix + key, field, (err, value) => {
-		if (!err && value !== null) {
-			this.stats.hits++;
-			return callback(null, JSON.parse(value));
-		}
-		this.stats.misses++;
-		if (this.next !== null) {
-			return this.next.get(key, field, (next_err, value) => {
-				if (next_err) {
-					return callback(next_err);
-				}
-				if (value === null) {
-					return callback(err, null);
-				}
-				this.client.hset(this.prefix + key, field, JSON.stringify(value), () => {
-					callback(null, value);
-				});
-			});
-		}
-		callback(err, null);
-	});
-};
-/**
- *
- * @param key
- * @param field
- * @param value
- * @param callback
- */
-Rush.prototype.set = function (key, field, value, callback) {
-	this.client.hset(this.prefix + key, field, JSON.stringify(value), (err) => {
-		if (this.next !== null) {
-			return this.next.set(key, field, value, callback);
-		}
-		callback(err);
-	});
-};
-/**
- *
- * @param key
- * @param field
- * @param callback
- */
-Rush.prototype.delete = function (key, field, callback) {
-	this.client.hdel(this.prefix + key, field, (err) => {
-		if (this.next !== null) {
-			return this.next.delete(key, field, callback);
-		}
-		callback(err, null);
-	});
-};
-/**
- * @param [propagate]
- * @param callback
- * @todo implement
- */
-Rush.prototype.clear = function (propagate, callback) {
-	if (typeof propagate === "function") {
-		callback = propagate;
-		propagate = undefined;
-	}
-	const stream = this.client.scanStream({match: this.prefix + "*"});
-	stream.on("data", (resultKeys) => {
-		this.client.del(resultKeys.join(' '));
-	});
-	stream.on("end", () => {
-		if (propagate && this.next) {
-			return this.next.clear(propagate, callback);
-		}
-		callback(null, true);
-	});
-};
-/**
- * @param [clear]
- * @returns {{hits: number, misses: number, ratio: number}}
- */
-Rush.prototype.getStats = function (clear) {
-	const stats = this.stats;
-	stats.ratio = stats.hits && stats.misses ? stats.hits / stats.misses : 0;
-	if (clear) {
-		this.clearStats();
-	}
-	return stats;
-};
-/**
- *
- */
-Rush.prototype.clearStats = function () {
-	this.stats = {
-		hits: 0,
-		misses: 0
-	};
-};
 module.exports = Rush;
--- test/base.js
+++ test/base.js
@@ -1,76 +1,76 @@
 /* global describe, it */
-'use strict';
+"use strict";
 /**
  * @author Michał Żaloudik <ponury.kostek@gmail.com>
  */
-const AMule = require('amule');
-const Rush = require('../');
-const Redis = require('ioredis');
+const AMule = require("amule");
+const Rush = require("../");
+const Redis = require("ioredis");
 const redisClient = new Redis();
-const redisClient2 = new Redis({db:2});
-const assert = require('assert');
-describe('Base', () => {
-	describe('AMule', () => {
+const redisClient2 = new Redis({db: 2});
+const assert = require("assert");
+describe("Base", () => {
+	describe("AMule", () => {
 		beforeEach((done) => {
 			redisClient.flushall(() => {
 				done();
 			});
 		});
-		it('has', (done) => {
+		it("has", (done) => {
 			let mule = new AMule();
 			mule.use(new Rush());
-			mule.has('key', 'field', function (err, has) {
+			mule.has("key", "field", function (err, has) {
 				assert.strictEqual(err, null);
 				assert.strictEqual(has, false);
-				mule.set('key', 'field', 'value', (err) => {
+				mule.set("key", "field", "value", (err) => {
 					assert.strictEqual(err, null);
-					mule.has('key', 'field', function (err, has) {
+					mule.has("key", "field", function (err, has) {
 						assert.strictEqual(err, null);
 						assert.strictEqual(has, true);
 						done();
-					})
+					});
 				});
 			});
 		});
-		it('set', (done) => {
+		it("set", (done) => {
 			let mule = new AMule();
 			mule.use(new Rush());
-			mule.set('key', 'field', 'value', (err) => {
+			mule.set("key", "field", "value", (err) => {
 				assert.strictEqual(err, null);
-				mule.has('key', 'field', (err, has) => {
+				mule.has("key", "field", (err, has) => {
 					assert.strictEqual(err, null);
 					assert.strictEqual(has, true);
 					done();
 				});
 			});
 		});
-		it('get', (done) => {
+		it("get", (done) => {
 			let mule = new AMule();
 			mule.use(new Rush());
-			mule.get('key', 'field', function (err, value) {
+			mule.get("key", "field", function (err, value) {
 				assert.strictEqual(err, null);
 				assert.strictEqual(value, null);
-				mule.set('key', 'field', 'value', (err) => {
+				mule.set("key", "field", "value", (err) => {
 					assert.strictEqual(err, null);
-					mule.get('key', 'field', function (err, val) {
+					mule.get("key", "field", function (err, val) {
 						assert.strictEqual(err, null);
-						assert.strictEqual(val, 'value');
+						assert.strictEqual(val, "value");
 						done();
-					})
+					});
 				});
 			});
 		});
-		it('delete', (done) => {
+		it("delete", (done) => {
 			let mule = new AMule();
 			mule.use(new Rush());
-			mule.set('key', 'field', 'value', (err) => {
+			mule.set("key", "field", "value", (err) => {
 				assert.strictEqual(err, null);
-				mule.has('key', 'field', (err, has) => {
+				mule.has("key", "field", (err, has) => {
 					assert.strictEqual(err, null);
 					assert.strictEqual(has, true);
-					mule.delete('key', 'field', function (err) {
+					mule.delete("key", "field", function (err) {
 						assert.strictEqual(err, null);
-						mule.has('key', 'field', (err, has) => {
+						mule.has("key", "field", (err, has) => {
 							assert.strictEqual(err, null);
 							assert.strictEqual(has, false);
 							done();
@@ -79,17 +79,17 @@ describe('Base', () => {
 				});
 			});
 		});
-		it('clear', (done) => {
+		it("clear", (done) => {
 			let mule = new AMule();
 			mule.use(new Rush());
-			mule.set('key', 'field', 'value', (err) => {
+			mule.set("key", "field", "value", (err) => {
 				assert.strictEqual(err, null);
-				mule.has('key', 'field', (err, has) => {
+				mule.has("key", "field", (err, has) => {
 					assert.strictEqual(err, null);
 					assert.strictEqual(has, true);
 					mule.clear(function (err) {
 						assert.strictEqual(err, null);
-						mule.has('key', 'field', (err, has) => {
+						mule.has("key", "field", (err, has) => {
 							assert.strictEqual(err, null);
 							assert.strictEqual(has, false);
 							done();
@@ -98,22 +98,22 @@ describe('Base', () => {
 				});
 			});
 		});
-		it('stats', (done) => {
+		it("stats", (done) => {
 			let mule = new AMule();
 			const rush = new Rush();
 			mule.use(rush);
-			mule.get('key', 'field', function (err, value) {
+			mule.get("key", "field", function (err, value) {
 				assert.strictEqual(err, null);
 				assert.strictEqual(value, null);
 				const stats = rush.getStats();
 				assert.strictEqual(stats.misses, 1);
 				assert.strictEqual(stats.ratio, 0);
 				assert.strictEqual(stats.hits, 0);
-				mule.set('key', 'field', 'value', (err) => {
+				mule.set("key", "field", "value", (err) => {
 					assert.strictEqual(err, null);
-					mule.get('key', 'field', function (err, val) {
+					mule.get("key", "field", function (err, val) {
 						assert.strictEqual(err, null);
-						assert.strictEqual(val, 'value');
+						assert.strictEqual(val, "value");
 						let stats = rush.getStats(true);
 						assert.strictEqual(stats.misses, 1);
 						assert.strictEqual(stats.ratio, 1);
@@ -123,12 +123,12 @@ describe('Base', () => {
 						assert.strictEqual(stats.ratio, 0);
 						assert.strictEqual(stats.hits, 0);
 						done();
-					})
+					});
 				});
 			});
 		});
 	});
-	describe('AMule 2 levels', () => {
+	describe("AMule 2 levels", () => {
 		beforeEach((done) => {
 			redisClient.flushall(() => {
 				redisClient2.flushall(() => {
@@ -136,69 +136,69 @@ describe('Base', () => {
 				});
 			});
 		});
-		it('has', (done) => {
-			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db:2});
+		it("has", (done) => {
+			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db: 2});
 			mule.use(rush1);
 			mule.use(rush2);
-			rush2.has('key', 'field', function (err, has) {
+			rush2.has("key", "field", function (err, has) {
 				assert.strictEqual(err, null);
 				assert.strictEqual(has, false);
-				mule.set('key', 'field', 'value', (err) => {
+				mule.set("key", "field", "value", (err) => {
 					assert.strictEqual(err, null);
-					rush2.has('key', 'field', function (err, has) {
+					rush2.has("key", "field", function (err, has) {
 						assert.strictEqual(err, null);
 						assert.strictEqual(has, true);
 						done();
-					})
+					});
 				});
 			});
 		});
-		it('set', (done) => {
-			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db:2});
+		it("set", (done) => {
+			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db: 2});
 			mule.use(rush1);
 			mule.use(rush2);
-			mule.set('key', 'field', 'value', (err) => {
+			mule.set("key", "field", "value", (err) => {
 				assert.strictEqual(err, null);
-				rush2.has('key', 'field', (err, has) => {
+				rush2.has("key", "field", (err, has) => {
 					assert.strictEqual(err, null);
 					assert.strictEqual(has, true);
 					done();
 				});
 			});
 		});
-		it('get', (done) => {
-			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db:2});
+		it("get", (done) => {
+			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db: 2});
 			mule.use(rush1);
 			mule.use(rush2);
-			rush2.get('key', 'field', function (err, value) {
+			rush2.get("key", "field", function (err, value) {
 				assert.strictEqual(err, null);
 				assert.strictEqual(value, null);
-				rush2.set('key', 'field', 'value', (err) => {
+				rush2.set("key", "field", "value", (err) => {
 					assert.strictEqual(err, null);
-					mule.get('key', 'field', function (err, val) {
+					mule.get("key", "field", function (err, val) {
 						assert.strictEqual(err, null);
-						assert.strictEqual(val, 'value');
+						assert.strictEqual(val, "value");
 					});
-					mule.get('key', 'field', function (err, val) {
+					mule.get("key", "field", function (err, val) {
 						assert.strictEqual(err, null);
-						assert.strictEqual(val, 'value');
+						assert.strictEqual(val, "value");
 						done();
-					})
+					});
 				});
 			});
 		});
-		it('delete', (done) => {
-			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db:2});
+		it("delete", (done) => {
+			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db: 2});
 			mule.use(rush1);
 			mule.use(rush2);
-			mule.set('key', 'field', 'value', (err) => {
+			mule.set("key", "field", "value", (err) => {
 				assert.strictEqual(err, null);
-				rush2.has('key', 'field', (err, has) => {
+				rush2.has("key", "field", (err, has) => {
 					assert.strictEqual(err, null);
 					assert.strictEqual(has, true);
-					mule.delete('key', 'field', function (err) {
+					mule.delete("key", "field", function (err) {
 						assert.strictEqual(err, null);
-						rush2.has('key', 'field', (err, has) => {
+						rush2.has("key", "field", (err, has) => {
 							assert.strictEqual(err, null);
 							assert.strictEqual(has, false);
 							done();
@@ -207,18 +207,18 @@ describe('Base', () => {
 				});
 			});
 		});
-		it('clear', (done) => {
-			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db:2});
+		it("clear", (done) => {
+			const mule = new AMule(), rush1 = new Rush(), rush2 = new Rush({db: 2});
 			mule.use(rush1);
 			mule.use(rush2);
-			mule.set('key', 'field', 'value', (err) => {
+			mule.set("key", "field", "value", (err) => {
 				assert.strictEqual(err, null);
-				rush2.has('key', 'field', (err, has) => {
+				rush2.has("key", "field", (err, has) => {
 					assert.strictEqual(err, null);
 					assert.strictEqual(has, true);
-					mule.clear(true, function (err) {
+					mule.clear(function (err) {
 						assert.strictEqual(err, null);
-						rush2.has('key', 'field', (err, has) => {
+						rush2.has("key", "field", (err, has) => {
 							assert.strictEqual(err, null);
 							assert.strictEqual(has, false);
 							done();
